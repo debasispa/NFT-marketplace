4c3177d68715ff3c8e1bd672968b859e0ba01135
debasispa <110556767+debasispa@users.noreply.github.com>
2024-06-09 09:12:24 +0530

    Add files via upload

---
+++ React-NFT-Website-getting-started/src/assets/data/NFT__DATA.js
@@ -0,0 +1,105 @@
+import img01 from "../images/img-01.jpg";
+import img02 from "../images/img-02.jpg";
+import img03 from "../images/img-03.jpg";
+import img04 from "../images/img-04.jpg";
+import img05 from "../images/img-05.jpg";
+import img06 from "../images/img-06.jpg";
+import img07 from "../images/img-07.jpg";
+import img08 from "../images/img-08.jpg";
+import img09 from "../images/img-09.jpg";
+import ava02 from "../images/ava-02.png";
+import ava03 from "../images/ava-03.png";
+import ava04 from "../images/ava-04.png";
+import ava05 from "../images/ava-05.png";
+import ava06 from "../images/ava-06.png";
+
+
+export const NFT__DATA = [
+  {
+    id: "01",
+    title: "Travel Monkey Club",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    imgUrl: img01,
+    creator: "Trisha Francis",
+    currentBid: 5.89,
+  },
+
+  {
+    id: "02",
+    title: "Sir Lion Swag",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    imgUrl: img02,
+    creator: "Trisha Francis",
+    creatorImg: ava02,
+    currentBid: 5.09,
+  },
+
+  {
+    id: "03",
+    title: "Civilian",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    imgUrl: img03,
+    creator: "Trisha Francis",
+    creatorImg: ava03,
+    currentBid: 6.89,
+  },
+
+  {
+    id: "04",
+    title: "Guard",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    imgUrl: img04,
+    creator: "Trisha Francis",
+    creatorImg: ava04,
+    currentBid: 7.89,
+  },
+
+  {
+    id: "05",
+    title: "Travel Monkey Club",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    creator: "Trisha Francis",
+    creatorImg: ava05,
+    currentBid: 4.89,
+  },
+
+  {
+    id: "06",
+    title: "Sir Lion Swag",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    imgUrl: img06,
+    creator: "Trisha Francis",
+    creatorImg: ava06,
+    currentBid: 4.99,
+  },
+
+  {
+    id: "07",
+    title: "Civilian",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    imgUrl: img07,
+    creator: "Trisha Francis",
+    creatorImg: ava03,
+    currentBid: 5.89,
+  },
+
+  {
+    id: "08",
+    title: "Guard",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    imgUrl: img08,
+    creator: "Trisha Francis",
+    creatorImg: ava04,
+    currentBid: 5.89,
+  },
+
+  {
+    id: "09",
+    title: "Travel Monkey Club",
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    imgUrl: img09,
+    creator: "Trisha Francis",
+    creatorImg: ava05,
+    currentBid: 5.89,
+  },
+];
---
+++ React-NFT-Website-getting-started/src/assets/data/data.js
@@ -0,0 +1,153 @@
+
+import ava01 from "../images/ava-01.png";
+import ava02 from "../images/ava-02.png";
+import ava03 from "../images/ava-03.png";
+import ava04 from "../images/ava-04.png";
+import ava05 from "../images/ava-05.png";
+import ava06 from "../images/ava-06.png";
+
+export const SELLER__DATA = [
+  {
+    id: "01",
+    desc: " I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    sellerName: "Ryan Carder",
+    sellerImg: ava01,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "02",
+    desc: " I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    sellerName: "Triste Francis",
+    sellerImg: ava02,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "03",
+    desc:  "I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+    sellerName: "Ryan Carder",
+    sellerImg: ava03,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "04",
+
+    desc: "It is very important for the customer to pay attention to the adipiscing process. I will come to receive the services of desire",
+
+    sellerName: "Ryan Carder",
+    sellerImg: ava04,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "05",
+
+    desc: "the coast itself loves to be pained, it will be followed by adipiscing. I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+
+    sellerName: "Trisha Francis",
+    sellerImg: ava05,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "06",
+
+    desc: " I wish to gain office by desire, but ours and those who have been corrupted by the smallest of easy labors do not know that there is no error that is born and often takes it as a reason for pains at the time. Rejected, and pleasures of the like kind, he repulses them with pain.",
+
+    sellerName: "Trisha Francis",
+    sellerImg: ava06,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "01",
+    desc: "",
+    sellerName: "Ryan Carder",
+    sellerImg: ava01,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "02",
+    desc: "It is very important for the customer to pay attention to the adipiscing process. I will come to receive the services of desire",
+    sellerName: "Trisha Francis",
+    sellerImg: ava02,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "03",
+    desc: "It is very important for the customer to pay attention to the advising process. I will come to receive the services of desire",
+    sellerName: "Ryan Carder",
+    sellerImg: ava03,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "04",
+
+    desc: "It is very important for the customer to pay attention to the advising process. I will come to receive the services of desire",
+
+    sellerName: "Ryan Carder",
+    sellerImg: ava04,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "05",
+
+    desc: "It is very important for the customer to pay attention to the advising process. I will come to receive the services of desire",
+
+    sellerName: "Trisha Francis",
+    sellerImg: ava05,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "06",
+
+    desc: "It is very important for the customer to pay attention to the adipiscing process. I will come to receive the services of desire",
+
+    sellerName: "Trisha Francis",
+    sellerImg: ava06,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instrUrl: "#",
+    twitUrl: "#",
+  },
+];
